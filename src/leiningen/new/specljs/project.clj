7c7c0f19945f66350cd84e69ead20482a440ed67
Micah <micahmartin@gmail.com>
2014-08-13 15:20:11 -0500

    updating for 3.1.0

--- src/leiningen/new/specljs/project.clj
+++ src/leiningen/new/specljs/project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
 
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-2014"]
-                                  [speclj "3.0.0"]]}}
-  :plugins [[speclj "3.0.0"]
-            [lein-cljsbuild "1.0.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-2234"]
+                                  [speclj "3.1.0"]]}}
+  :plugins [[speclj "3.1.0"]
+            [lein-cljsbuild "1.0.3"]]
 
   :cljsbuild {:builds        {:dev  {:source-paths   ["src/cljs" "spec/cljs"]
                                      :compiler       {:output-to     "js/{{name}}_dev.js"
